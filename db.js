5801bf7f14099cbe4d8d66607f76ece19d039556
NevMem <memlolkek@gmail.com>
2018-08-31 16:43:05 +0300

    Some commit a lot of chanegs a lot ready for work

---
+++ db.js
@@ -0,0 +1,102 @@
+const mongodb = require('mongodb'), 
+    jwt = require('jsonwebtoken'), 
+    fs = require('fs')
+require('dotenv').config()
+
+const MongoClient = mongodb.MongoClient
+let db_url = process.env.db_url.replace('<dbuser>', process.env.db_user).replace('<dbpassword>', process.env.db_password)
+
+let db = undefined
+
+exports.connect = () => {
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(db_url, { useNewUrlParser: true }, (err, client) => {
+            if (err) {
+                reject(err)
+            } else {
+                db = client.db(process.env.db_name)
+                resolve()
+            }
+        })
+    })
+}
+
+exports.get_data = (user_token, login) => {
+    return new Promise((resolve, reject) => {
+        db.collection('data').aggregate([ { $match: { login: login } }, { $sort: { timestamp: -1 } } ]).toArray((err, data) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(data)
+            }
+        })
+    })
+}
+
+exports.add = (token, login, date, name, wallet, value, tags) => {
+    let timestamp = date.minute + date.hour * 60 + date.day * 24 * 60 + date.month * 31 * 24 * 60 + date.year * 12 * 31 * 24 * 60
+    return new Promise((resolve, reject) => {
+        db.collection('data').insert({ login: login, date: date, name: name, wallet: wallet, value: value, timestamp: timestamp, tags: tags }).then(data => {
+            console.log(data)
+            resolve('ok')
+        })
+        .catch(err => {
+            reject(err)
+        })
+    })
+}
+
+exports.login = ( login, password ) => {
+    return new Promise((resolve, reject) => {
+        db.collection('users').find({ login: login }).toArray((err, response) => {
+            if (err) {
+                reject(err)
+            } else {
+                if (response.length >= 2) {
+                    console.log('Corrupted database'.red)
+                    reject('Error occured please connect us')
+                } else {
+                    if (response.length == 0) {
+                        reject('User not found')
+                    } else {
+                        if (response[0].password === password) {
+                            let token = jwt.sign({ login: login, first_name: response[0].first_name, last_name: response[0].last_name }, process.env.jwt_secret)
+                            resolve({ token: token, login: login, first_name: response[0].first_name, last_name: response[0].last_name })
+                        } else {
+                            reject('Wrong login or password')
+                        }
+                    }
+                }
+            }
+        })
+    })
+}
+
+clearAll = () => {
+    console.log('Droping table')
+    return new Promise((resolve, reject) => {
+        db.collection('data').deleteMany({}).then(data => {
+            console.log(data.result.n + ' rows was deleted')
+            resolve()
+        })
+    })
+}
+
+fillDefault = () => {
+    return new Promise((resolve, reject) => {
+        let data = JSON.parse(fs.readFileSync('start.json', 'utf-8'))
+        console.log(data.length)
+        db.collection('data').insertMany(data).then(res => {
+            console.log(res.result.n + ' was inserted')
+            resolve()
+        })
+    })
+}
+
+exports.debug = () => {
+    clearAll().then(() => {
+        return fillDefault()
+    }).then(() => {
+        console.log('here')
+    })
+}
---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require('express'), 
+    mongodb = require('mongodb'), 
+    bParser = require('body-parser'), 
+    jwt = require('jsonwebtoken'), 
+    db = require('./db')
+
+require('colors')
+require('dotenv').config()
+
+db.connect().then(() => {
+    let app = express()
+
+    app.use(bParser.json())
+    app.use(bParser.urlencoded({ extended: true }))
+
+    app.use((req, res, next) => {
+        console.log((`[${Date()}] ${req.method} ${req.url}`).cyan)
+
+        next()
+    })
+
+    app.post('/api/login', (req, res) => {
+        let login = req.body.login, password = req.body.password
+        console.log(('Try to log in with login: ' + login + ' password: ' + '---//---').cyan)
+        
+        db.login(login, password)
+        .then(response => {
+            res.send(response)
+        })
+        .catch(err => {
+            console.log(err)
+            res.send({ err: err })
+        })
+    })
+
+    app.post('/api/add', (req, res) => {
+        let token = req.body.token, 
+            date = req.body.date, 
+            name = req.body.name, 
+            value = req.body.value, 
+            wallet = req.body.wallet, 
+            login = req.body.login, 
+            tags = req.body.tags
+
+        if (token) {
+            jwt.verify(token, process.env.jwt_secret, (err, decoded) => {
+                if (err) {
+                    res.send({ err: 'Token is ivalid or empty. Please relogin' })
+                } else {
+                    console.log(decoded)
+                    db.add(token, login, date, name, wallet, value, tags)
+                    .then(data => {
+                        res.send({ data })
+                    })
+                    .catch(err => {
+                        res.send({ err: err })
+                    })
+                }
+            })
+        } else {
+            res.send({ err: 'Token is ivalid or empty. Please relogin' })
+        }
+    })
+
+    app.post('/api/data', (req, res) => {
+        let token = req.body.token
+        let login = req.body.login
+
+        if (!token) {
+            res.send({ err: 'Token is ivalid or empty. Please relogin' })
+        } else {
+            db.get_data(token, login).then(data => {
+                res.send(data)
+            })
+            .catch(err => {
+                console.log(err)
+                res.send({ err: err })
+            })
+        }
+    })
+
+    app.listen(process.env.api_port, err => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log('Server is running'.green)
+        }
+    })
+        
+    db.debug()
+})
+.catch(err => {
+    console.log(err)
+})
