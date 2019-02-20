5480589982b28ae7c490e7da193b738f53499a21
NevMem <memlolkek@gmail.com>
2019-02-20 11:33:25 +0300

    Added 404 error handling

--- index.js
+++ index.js
@@ -501,6 +501,11 @@ db.connect().then(() => {
         })
     })
 
+    app.use((req, res, next) => {
+        res.status(404).send('<h1>404 error path not found</h1>')
+        next()
+    })
+
     app.listen(process.env.api_port, err => {
         if (err) {
             console.log(err)
